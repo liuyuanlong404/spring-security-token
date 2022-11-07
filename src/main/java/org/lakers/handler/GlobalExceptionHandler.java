b074c5c855b82f0e82a2813d3bc44028b0fc4167
Lakers <1501862490@qq.com>
2022-11-07 09:36:03 +0800

    全局异常处理器添加 AccessDeniedException.class 异常处理

--- src/main/java/org/lakers/handler/GlobalExceptionHandler.java
+++ src/main/java/org/lakers/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package org.lakers.handler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.lakers.domain.ResponseResult;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,21 +17,26 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = BizException.class)
-    public <T> ResponseResult<T> bizException(BizException e){
+    public <T> ResponseResult<T> bizException(BizException e) {
         log.error(e.getMessage(), e);
         return ResponseResult.error(e.getErrorCode(), e.getErrorMsg());
     }
 
     @ExceptionHandler(value = HttpRequestMethodNotSupportedException.class)
-    public <T> ResponseResult<T> httpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e){
+    public <T> ResponseResult<T> httpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
         log.error(e.getMessage(), e);
         return ResponseResult.error(202, "请求方式有误！");
     }
 
     @ExceptionHandler(value = Exception.class)
-    public <T> ResponseResult<T> exception(Exception e){
+    public <T> ResponseResult<T> exception(Exception e) {
         log.error(e.getMessage(), e);
         return ResponseResult.error(e.getMessage());
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public void accessDeniedException(AccessDeniedException e) {
+        throw e;
     }
 
 }
